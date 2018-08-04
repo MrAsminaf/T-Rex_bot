67c32323b8002308c53a7a7a3c0bcec8c814f1af
unknown <mrasminaf@gmail.com>
2018-08-04 22:14:54 +0200

    Deleted unnecessary code

--- bot.py
+++ bot.py
@@ -1,27 +1,21 @@
 import pyautogui
-import PIL
+from PIL import ImageGrab, ImageOps
 from numpy import *
-import time
-
-class Cords:
-	replayBt = [950, 550]
-
-def ClickReplBt():
-	pyautogui.click(Cords.replayBt[0], Cords.replayBt[1])
 
 def Jump():
 	pyautogui.keyDown('space')
 
-def ImageGrab():
-	box = [790, 480, 820, 520]                         
-	image = PIL.ImageGrab.grab(box)
-	grayImage = PIL.ImageOps.grayscale(image)
-	a = array(grayImage.getcolors())
-	return a.sum()
+def ImageSum():
+    box = [770, 380, 850, 450]
+    image = ImageGrab.grab(box)
+    grayImage = ImageOps.grayscale(image)
+    a = array(grayImage.getcolors())
+    print(a.sum())
+    return a.sum()
 
-def main(): 
+def main():
 	while True:
-		if ImageGrab () > 2000:
+		if ImageSum () > 6000:
 			Jump()
 
 if __name__ == "__main__":
